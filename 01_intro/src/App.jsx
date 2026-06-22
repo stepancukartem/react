283d99148521e3758f5acd9a9f2b20071bf58f1b
Степанчук Артем Миколайович <stepa_am11@student.itstep.org>
2026-06-22 20:40:49 +0300

    first commit

---
+++ 01_intro/src/App.jsx
@@ -0,0 +1,70 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+
+      <h1>Моє місто — Рівне</h1>
+      <p><b>Країна:</b> Україна</p>
+      <p><b>Рік заснування:</b> 1283</p>
+
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Rivne_Theatre_Square.jpg"
+        alt="Рівне"
+      />
+
+      <hr />
+
+      <h1>Улюблений фільм</h1>
+      <p><b>Назва:</b> Форсаж 7</p>
+      <p><b>Режисер:</b> Джеймс Ван</p>
+      <p><b>Рік випуску:</b> 2015</p>
+      <p><b>Кіностудія:</b> Universal Pictures</p>
+
+      <img
+        src="https://upload.wikimedia.org/wikipedia/en/b/b8/Furious_7_poster.jpg"
+        alt="Форсаж 7"
+      />
+
+      <hr />
+
+      <h1>Домашній улюбленець</h1>
+      <p><b>Кличка:</b> Джус</p>
+      <p><b>Вид:</b> Собака</p>
+      <p><b>Вік:</b> 17 років</p>
+
+      <img
+        src="https://images.dog.ceo/breeds/husky/n02110185_1469.jpg"
+        alt="Джус"
+      />
+
+      <hr />
+
+      <h1>Улюблений музичний гурт</h1>
+      <p><b>Назва:</b> Кіно</p>
+
+      <h3>Учасники:</h3>
+      <ul>
+        <li>Віктор Цой</li>
+        <li>Юрій Каспарян</li>
+        <li>Ігор Тихомиров</li>
+        <li>Георгій Гур'янов</li>
+      </ul>
+
+      <h3>Відомі альбоми:</h3>
+      <ul>
+        <li>Группа крови</li>
+        <li>Звезда по имени Солнце</li>
+        <li>Ночь</li>
+      </ul>
+
+      <img
+        src="https://upload.wikimedia.org/wikipedia/ru/4/45/%D0%97%D0%B2%D0%B5%D0%B7%D0%B4%D0%B0_%D0%BF%D0%BE_%D0%B8%D0%BC%D0%B5%D0%BD%D0%B8_%D0%A1%D0%BE%D0%BB%D0%BD%D1%86%D0%B5.jpg"
+        alt="Кіно"
+      />
+
+    </div>
+  );
+}
+
+export default App;
